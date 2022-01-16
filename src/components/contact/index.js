9c5fb5996cd209b77c492fa2f7b9c8eefe3fd8b0
Noah Tidwell <ntidwell34@yahoo.com>
2022-01-16 11:16:23 -0600

    update Contact with Github, LinkedIn, Email, and Mobile

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './contact.css'
 
 function Contact() {
     return (
-        <div>
-            CONTACT ME
+        <div className='contact_container' id="contact">
+            <h1>MY INFO</h1>
+            <ul>
+  <li><a target='_blank' rel='noreferrer' href="https://github.com/NoahTidwell">Github</a> / <a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/noah-tidwell-b746b3206/">LinkedIn</a></li>
+  <li><a target='_blank' rel='noreferrer' href="mailto:ntidwell34@yahoo.com">Email</a></li>
+  <li><a target='_blank' rel='noreferrer' href="tel:+18324616104">832-461-6104</a></li>
+</ul>
         </div>
     )
 }
