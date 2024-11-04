d2c3f9530effcb717f6e81092fa9e62291b8eecb
Noah Tidwell <ntidwell34@yahoo.com>
2024-11-04 13:59:15 -0600

    about me changes

--- src/components/about-me/index.jsx
+++ src/components/about-me/index.jsx
@@ -13,7 +13,7 @@ function AboutMe() {
                 <p> 
                 Hello and welcome to my page! My name is Noah Tidwell. I am currently a student at University of Houston studying for a BA in Journalism. I enjoy learning about MERN Stack Development and am enthusiastic about blockchain.
 
-In my pasttime, I enjoy playing the piano and guitar, hiking in the mountains, and gardening. I research blockchain projects and have professional written about many different blockchain projects. I am currently working on a project that will help people learn about blockchain and how to participate in it.
+In my pasttime, I play the piano, guitar and love hiking in the mountains.
                 </p>
             </div>
         </div>
