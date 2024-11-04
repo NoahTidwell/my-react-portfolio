d4772d9f285dd825f20d94ec6ad6720742e99ba4
Noah Tidwell <ntidwell34@yahoo.com>
2024-11-04 13:26:19 -0600

    test 2024

--- src/components/about-me/index.jsx
+++ src/components/about-me/index.jsx
@@ -11,7 +11,7 @@ function AboutMe() {
             <div className='about_text'>
                 <h1>ABOUT ME</h1>
                 <p> 
-                Hello and welcome to my page! My name is Noah Tidwell. I am a 23 year old student learning MERN Stack Development. I am enthusiastic about building dynamic websites for users to have an engaging experience with. I am interested in Web3 and Blockchains.
+                Hello and welcome to my page! My name is Noah Tidwell. I currently a student at University of Houston, Main Campus studying for a BA in Journalism. In my pastime I enjoy learning MERN Stack Development. I am enthusiastic about building dynamic websites for users to have an engaging experience with. I am interested in web 3 and blockchain Development.
 
 In my pasttime, I enjoy playing the piano and guitar, hiking in the mountains, and gardening. I research blockchain projects and help curate a daily newsletter on the Solana Blockchain known as the <a target='_blank' rel='noreferrer' href='https://thesolanagrapevine.substack.com'>Solana Grapevine</a>
                 </p>
